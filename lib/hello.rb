f97460fc861d1158be25635d531ca4ba881aab63
Madeline Celis <madelinebcelis@gmail.com>
2018-07-23 16:30:43 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,16 +6,18 @@ def hello_t(array)
       yield(array[i])
       i += 1
     end
-    
+
   array
   else
-    puts "Hey! No block was given"
+    puts "Hey! No block was given!"
   end
 end
 
 #the yield is a anonymous function callback taht you are passing to your
 #original array that do part starts the anonymous
-#function
+#function above will throw an error if you do not
+# make an if statement allowing for no block to be passed in case
+# someone forgets to pass the anonymous function
 hello_t(['Tim', 'Tom', "Jim"]) do
   |name|
   if name.start_with?('T')
